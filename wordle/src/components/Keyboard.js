4e778de1d76cde7987fd7d1eb39ab565f1061989
Olga Murashova <om.dreyfus@gmail.com>
2023-02-19 22:09:40 -0500

    added key.js and updated keyboard.js

--- wordle/src/components/Keyboard.js
+++ wordle/src/components/Keyboard.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 const Keyboard = () => {
+  const keys1 = ["Q", "W","E","R","T","Y","U","I","O","P"];
+  const keys2 = ["A","S","D","F","G","H","J","K","L"];
+  const keys3 = ["Z","X","C","V","B","N","M"];
+
   return (
-    <div>
+ 
+
+
+
+    <div className='keyboard'>
+      <div className='line1'>
+        {keys1.map((key) => {
+          return <div> {key} </div>;
+        })}
+
+      </div>
+
+      <div className='line2'>
+
+      {keys2.map((key) => {
+          return <div> {key} </div>;
+        })}
+
+      </div>
+
+      <div className='line3'>
+        {keys3.map((key) => {
+          return <div> {key} </div>;
+        })}
+
+ 
+
+      </div>
       
     </div>
   );
