32c861ef0221641ffd0f6bad3e7ac4df55a8ebf1
Olga Murashova <om.dreyfus@gmail.com>
2023-02-20 15:58:42 -0500

    updated app.js

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -17,6 +17,36 @@ function App() {
 
   const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
 
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+
+
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+        setBoard(newBoard);
+
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+
+  };
+
+  const onDelete = () => {
+    if(currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos -1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos -1});
+
+  };
+
+
+  const onEnter = () => {
+    if(currAttempt.letterPos !==5) return;
+    setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+
+
+  };
+
+
   return <div className='App'>
     <nav> <h1>Wordle</h1></nav>
 
--- wordle/src/components/Key.js
+++ wordle/src/components/Key.js
@@ -7,20 +7,14 @@ const Key = ({keyVal, bigKey}) => {
 
     const selectLetter = () => {
         if(keyVal === "ENTER"){
-            if(currAttempt.letterPos !==5) return;
-            setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+           
+            
 
-        }else {
-
-
-        if (currAttempt.letterPos > 4) return;
-
-
-        const newBoard = [...board];
-        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-        setBoard(newBoard);
-
-        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+        } else if (keyVal === "DELETE"){
+          
+            
+        } else {
+   
 
         }
     };
